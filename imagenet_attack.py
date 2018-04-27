3165f601e920b2564c631c1226ebe4708007a355
joy8023 <joyjoy8023@gmail.com>
2018-04-28 00:25:00 +0800

    add 95 attack

--- imagenet_attack.py
+++ imagenet_attack.py
@@ -94,14 +94,14 @@ ut_label = []
 tar = []
 ut_tar = []
 
-samples = 10
-start = 0
+samples = 40
+start = 10
 confidence = 0
 bs = 9
 mi = 1000
 
 #classification+samples+batch_size+start
-filename = '95s10bs9.pkl'
+filename = '95s40bs9start10.pkl'
 utfile = 'ut_'+filename
 advname = 'adv_'+filename
 
